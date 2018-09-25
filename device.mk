6d49fd7d82948dbae538f877adc58bfff708b529
xyyx <xyyx@mail.ru>
2018-09-25 17:00:21 +0800

    Add health 2.0 service
    Change-Id: Id01cb4019982a05e2701a4c4ca6f0535ad136dc2
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- device.mk
+++ device.mk
@@ -219,8 +219,7 @@ PRODUCT_COPY_FILES += \
 
 # Healthd
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.0-impl \
-    android.hardware.health@2.0-service
+    android.hardware.health@2.0-service.gemini
 
 # HIDL
 PRODUCT_PACKAGES += \
